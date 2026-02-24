bf8181953feaf412c3d947d8d1dfb61d641032d4
Shreyas Thoke <153861852+Shreyas-212@users.noreply.github.com>
2026-02-24 14:51:01 +0530

    Add 'program' post type and update 'event' post type

--- mustUsePlugins.php
+++ mustUsePlugins.php
@@ -1,8 +1,9 @@
 <?php 
 function university_post_types() {
+    //Event post type
     register_post_type('event', array(
         'show_in_rest' => true,
-        'supports' => array('title', 'editor', 'excerpt', 'custom-fields'),
+        'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
         'public' => true,
@@ -15,6 +16,24 @@ function university_post_types() {
             'singular_name' => 'Event'
         ), 
         'menu_icon' => 'dashicons-calendar'
+    ));
+
+    //Program post type
+    register_post_type('program', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'programs'),
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ), 
+        'menu_icon' => 'dashicons-awards'
     ));
 }
 
